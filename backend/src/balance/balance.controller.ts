1310ad73a893fa844420c3c6b1aebcbb63eb3d04
Gonzalo Murua <gonzaloemurua96@gmail.com>
2025-05-03 19:30:19 -0300

    Modificaciones de algunos endpoint y de informacion para swagger

--- backend/src/balance/balance.controller.ts
+++ backend/src/balance/balance.controller.ts
@@ -1,17 +1,25 @@
 /* eslint-disable prettier/prettier */
 import { Controller, Get, Param } from '@nestjs/common';
 import { BalanceService } from './balance.service';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 
-@Controller('balances')
+@Controller()
+@ApiTags('Balance')
 export class BalanceController {
   constructor(private readonly balanceService: BalanceService) {}
 
-  @Get('group/:groupId')
+  @Get('groups/:groupId/balances')
+  @ApiOperation({
+    summary: 'Devuelve un listado esta de balance por usuario del grupo y pagos recomendados',
+  })
   async getBalancesByGroup(@Param('groupId') groupId: number) {
     return this.balanceService.calculateBalancesForGroup(groupId);
   }
 
-  @Get('group/:groupId/equal-division')
+  @Get('groups/:groupId/equal-division')
+  @ApiOperation({
+    summary: 'Devuelve un listado esta de balance por usuario del grupo y pagos recomendados',
+  })
   async getEqualDivisionByGroup(@Param('groupId') groupId: number){
     return this.balanceService.calculateEqualDivision(groupId);
   }
