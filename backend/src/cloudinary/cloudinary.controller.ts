64e7e560d5351855eaa90aa2ae2642d5e55e65b5
Chesar <cesargarcialeoc@gmail.com>
2025-05-05 10:15:50 -0300

    update: image upload added

--- backend/src/cloudinary/cloudinary.controller.ts
+++ backend/src/cloudinary/cloudinary.controller.ts
@@ -12,21 +12,19 @@ import {
 } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { CloudinaryService } from './cloudinary.service';
-import { ProductsService } from '../products/products.service';
 import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
-import { AuthGuard } from '../auth/guards/auth.guard.middleware';
+import { AuthenticationGuard } from '../auth/guards/authentication/authentication.guard';
 
 @ApiTags('Images')
 @Controller()
 export class CloudinaryController {
   constructor(
-    private readonly cloudinaryService: CloudinaryService,
-    private readonly productsService: ProductsService,
+    private readonly cloudinaryService: CloudinaryService
   ) {}
 
   @ApiBearerAuth()
-  @UseGuards(AuthGuard)
-  @Post('files/uploadImage/:id')
+  @UseGuards(AuthenticationGuard)
+  @Post('expenses/:id/image')
   @UseInterceptors(FileInterceptor('image'))
   async getUserImages(
     @UploadedFile() file: Express.Multer.File,
