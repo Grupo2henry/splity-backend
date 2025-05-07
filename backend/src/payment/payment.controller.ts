b13b2a000066c8e6f3c181bc848b9b636fa2fd72
tu-usuario <nicolas001alvarez@gmail.com>
2025-05-06 21:00:12 -0300

    antes de mandar a main

--- backend/src/payment/payment.controller.ts
+++ backend/src/payment/payment.controller.ts
@@ -1,5 +1,19 @@
+/* eslint-disable @typescript-eslint/no-unsafe-argument */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 /* eslint-disable prettier/prettier */
-import { Controller, Get, Post, Put, Delete, Param, Body, UseGuards, Req } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Put,
+  Delete,
+  Param,
+  Body,
+  UseGuards,
+  Req,
+} from '@nestjs/common';
 import { PaymentService } from './payment.service';
 import { CreatePaymentDto } from './dto/create-payment.dto';
 import { UpdatePaymentDto } from './dto/update-payment.dto';
@@ -14,7 +28,7 @@ import { PaymentNotificationDto } from './dto/payment-notification.dto';
 export class PaymentController {
   constructor(
     private readonly paymentService: PaymentService,
-    private readonly mercadoPagoService: MercadoPagoService
+    private readonly mercadoPagoService: MercadoPagoService,
   ) {}
 
   @Get()
@@ -23,12 +37,16 @@ export class PaymentController {
   }
 
   @Get(':id')
-  async findPayment(@Param('id') id: number): Promise<Payment | null | undefined> {
+  async findPayment(
+    @Param('id') id: number,
+  ): Promise<Payment | null | undefined> {
     return this.paymentService.findById(id);
   }
 
   @Post()
-  async createPayment(@Body() createPaymentDto: CreatePaymentDto): Promise<Payment> {
+  async createPayment(
+    @Body() createPaymentDto: CreatePaymentDto,
+  ): Promise<Payment> {
     return this.paymentService.create(createPaymentDto);
   }
 
@@ -42,11 +60,13 @@ export class PaymentController {
     const { status, paymentId, preferenceId } = paymentNotification;
 
     try {
-      const paymentDetails = await this.mercadoPagoService.getPaymentDetails(paymentId);
+      const paymentDetails =
+        await this.mercadoPagoService.getPaymentDetails(paymentId);
 
       if (!paymentDetails) {
         return {
-          message: 'No se pudieron obtener los detalles del pago de Mercado Pago',
+          message:
+            'No se pudieron obtener los detalles del pago de Mercado Pago',
           status,
           paymentId,
           preferenceId,
@@ -56,14 +76,16 @@ export class PaymentController {
       const createPaymentDto: CreatePaymentDto = {
         userId: user.id,
         amount: paymentDetails.transaction_amount ?? 0,
-        payment_date: new Date(paymentDetails.date_approved || paymentDetails.date_created),
+        payment_date: new Date(
+          paymentDetails.date_approved || paymentDetails.date_created,
+        ),
         status: status as Payment['status'], // Usamos el tipo de la entidad
         transaction_id: paymentDetails.id?.toString(), // Intentamos obtener el ID de transacción
       };
 
       const payment = await this.paymentService.create(createPaymentDto); // Usamos el método create estándar
 
-      console.log("Pago guardado en la base de datos: ", payment)
+      console.log('Pago guardado en la base de datos: ', payment);
 
       return {
         message: 'Pago guardado correctamente',
@@ -79,7 +101,10 @@ export class PaymentController {
   }
 
   @Put(':id')
-  async updatePayment(@Param('id') id: number, @Body() updatePaymentDto: UpdatePaymentDto): Promise<Payment | undefined> {
+  async updatePayment(
+    @Param('id') id: number,
+    @Body() updatePaymentDto: UpdatePaymentDto,
+  ): Promise<Payment | undefined> {
     return this.paymentService.update(id, updatePaymentDto);
   }
 
