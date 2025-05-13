6e33c46e674e9be6632420ecc75f8aa471631884
tu-usuario <nicolas001alvarez@gmail.com>
2025-05-13 14:15:59 -0300

    antes de develop 2

--- backend/src/config/typeorm.ts
+++ backend/src/config/typeorm.ts
@@ -20,13 +20,13 @@ if (process.env.DROP_SCHEMA === 'false') {
 
 export const dbConfig: TypeOrmModuleOptions = {
   type: 'postgres',
-  url: process.env.DATABASE_PUBLIC_URL,
-  // database: process.env.DB_NAME,
-  // host: process.env.DB_HOST,
-  // port: Number(process.env.DB_PORT),
-  // username: process.env.DB_USERNAME,
-  // password: process.env.DB_PASSWORD,
-  // ///
+  // url: process.env.DATABASE_PUBLIC_URL,
+  database: process.env.DB_NAME,
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  ///
   autoLoadEntities: true,
   dropSchema: dropSchemaDB, //Poner en true llegado el caso
   synchronize: true,
