7ddcd46ce308dac6795b7096c09116c05a79f5de
Gonzalo Murua <gonzaloemurua96@gmail.com>
2025-05-17 15:16:31 -0300

    Minimos cambis por errores en la rama local

---
+++ backend/src/group/dto/get-groups.dto.ts
@@ -0,0 +1,34 @@
+import {
+  IsString,
+  IsOptional,
+  IsInt,
+  Min,
+  IsDateString,
+} from 'class-validator';
+import { Type } from 'class-transformer';
+
+export class GetGroupsDto {
+  @IsOptional()
+  @Type(() => Number)
+  @IsInt()
+  @Min(1)
+  page?: number = 1;
+
+  @IsOptional()
+  @Type(() => Number)
+  @IsInt()
+  @Min(1)
+  limit?: number = 6;
+
+  @IsOptional()
+  @IsString()
+  search?: string = '';
+
+  @IsOptional()
+  @IsDateString()
+  startDate?: string;
+
+  @IsOptional()
+  @IsDateString()
+  endDate?: string;
+}
--- backend/src/group/entities/group.entity.ts
+++ backend/src/group/entities/group.entity.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unsafe-return */
 /* eslint-disable prettier/prettier */
 import {
   Entity,
@@ -44,5 +45,4 @@ export class Group {
 
   @OneToMany(() => Expense, (expense) => expense.group)
   expenses: Expense[];
-
 }
