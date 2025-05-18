44d3803484db77ef5f60cf44a3179e6ac15fee82
tu-usuario <nicolas001alvarez@gmail.com>
2025-05-18 02:04:11 -0300

    r

--- backend/src/expenses/expenses.service.ts
+++ backend/src/expenses/expenses.service.ts
@@ -1,13 +1,28 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/no-unsafe-argument */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
 /* eslint-disable prettier/prettier */
-import { Injectable, NotFoundException } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+import {
+  Between,
+  ILike,
+  LessThanOrEqual,
+  MoreThanOrEqual,
+  Repository,
+} from 'typeorm';
 import { Expense } from './entities/expense.entity';
 import { User } from '../user/entities/user.entity';
 import { Group } from '../group/entities/group.entity';
 import { CreateExpenseDto } from './dto/create-expense.dto';
 import { UserService } from '../user/user.service';
 import { GroupService } from '../group/services/group.service';
+import { GetExpensesDto } from './dto/get-expense.dto';
+import { format } from 'date-fns';
 
 @Injectable()
 export class ExpensesService {
@@ -18,16 +33,20 @@ export class ExpensesService {
     private readonly groupService: GroupService,
   ) {}
 
-  async findAll(): Promise<Expense[]>{
-    return this.expenseRepository.find()
+  async findAll(): Promise<Expense[]> {
+    return this.expenseRepository.find();
   }
 
   async getExpenses(groupId: string): Promise<Expense[]> {
-    return this.expenseRepository.find({ where: { group: { id: Number(groupId) } } });
+    return this.expenseRepository.find({
+      where: { group: { id: Number(groupId) } },
+    });
   }
 
   async getExpense(id: string): Promise<Expense> {
-    const expense = await this.expenseRepository.findOne({ where: { id: Number(id) } });
+    const expense = await this.expenseRepository.findOne({
+      where: { id: Number(id) },
+    });
     if (!expense) {
       throw new NotFoundException(`Expense with id ${id} not found`);
     }
@@ -36,9 +55,13 @@ export class ExpensesService {
 
   // Nuevo método para buscar un gasto por su descripción exacta
   async getExpenseByDescription(description: string): Promise<Expense> {
-    const expense = await this.expenseRepository.findOne({ where: { description } });
+    const expense = await this.expenseRepository.findOne({
+      where: { description },
+    });
     if (!expense) {
-      throw new NotFoundException(`Expense with description "${description}" not found`);
+      throw new NotFoundException(
+        `Expense with description "${description}" not found`,
+      );
     }
     return expense;
   }
@@ -48,13 +71,17 @@ export class ExpensesService {
       // Verificar que el usuario existe
       const user = await this.userService.findOne(dto.paid_by);
       if (!user) {
-        throw new NotFoundException(`Cannot create expense: User with id ${dto.paid_by} not found`);
+        throw new NotFoundException(
+          `Cannot create expense: User with id ${dto.paid_by} not found`,
+        );
       }
 
       // Verificar que el grupo existe
       const group = await this.groupService.findOne(groupId);
       if (!group) {
-        throw new NotFoundException(`Cannot create expense: Group with id ${groupId} not found`);
+        throw new NotFoundException(
+          `Cannot create expense: Group with id ${groupId} not found`,
+        );
       }
     } catch (error) {
       if (error instanceof NotFoundException) {
@@ -75,13 +102,18 @@ export class ExpensesService {
     return this.expenseRepository.save(expense);
   }
 
-  async updateExpense(id: string, updateData: Partial<CreateExpenseDto>): Promise<Expense> {
+  async updateExpense(
+    id: string,
+    updateData: Partial<CreateExpenseDto>,
+  ): Promise<Expense> {
     if (updateData.paid_by) {
       try {
         await this.userService.findOne(updateData.paid_by);
       } catch (error) {
         if (error instanceof NotFoundException) {
-          throw new NotFoundException(`Cannot update expense: User with id ${updateData.paid_by} not found`);
+          throw new NotFoundException(
+            `Cannot update expense: User with id ${updateData.paid_by} not found`,
+          );
         }
         throw error;
       }
@@ -89,13 +121,20 @@ export class ExpensesService {
 
     const transformedData: any = {
       ...updateData,
-      paid_by: updateData.paid_by ? { id: String(updateData.paid_by) } : undefined,
+      paid_by: updateData.paid_by
+        ? { id: String(updateData.paid_by) }
+        : undefined,
     };
 
-    const updateResult = await this.expenseRepository.update(id, transformedData);
+    const updateResult = await this.expenseRepository.update(
+      id,
+      transformedData,
+    );
 
     if (updateResult.affected === 0) {
-      throw new NotFoundException(`Expense with id ${id} not found or no changes were made`);
+      throw new NotFoundException(
+        `Expense with id ${id} not found or no changes were made`,
+      );
     }
 
     return this.getExpense(id);
@@ -105,6 +144,69 @@ export class ExpensesService {
     const result = await this.expenseRepository.delete(id);
     if (result.affected === 0) {
       throw new NotFoundException(`Expense with id ${id} not found`);
+    }
+  }
+  async getExpensesOfGroup(groupId: string, query: GetExpensesDto) {
+    const { page = 1, limit = 6, search = '', startDate, endDate } = query;
+    if (startDate && isNaN(new Date(startDate).getTime())) {
+      throw new BadRequestException('Invalid startDate format');
+    }
+    if (endDate && isNaN(new Date(endDate).getTime())) {
+      throw new BadRequestException('Invalid endDate format');
+    }
+    const where: any = {
+      group: { id: groupId },
+      active: true,
+    };
+
+    if (search) {
+      where.description = ILike(`%${search}%`);
+    }
+
+    if (startDate && endDate) {
+      const start = new Date(startDate);
+      start.setHours(0, 0, 0, 0);
+      const end = new Date(endDate);
+      end.setHours(23, 59, 59, 999);
+      where.created_at = Between(start, end);
+    } else if (startDate) {
+      const start = new Date(startDate);
+      start.setHours(0, 0, 0, 0);
+      where.created_at = MoreThanOrEqual(start);
+    } else if (endDate) {
+      const end = new Date(endDate);
+      end.setHours(23, 59, 59, 999);
+      where.created_at = LessThanOrEqual(end);
+    }
+
+    console.log('where', where);
+    try {
+      const exps = await this.expenseRepository.find({
+        where,
+        skip: (page - 1) * limit,
+        take: limit,
+      });
+
+      const total = await this.expenseRepository.count({
+        where,
+      });
+      console.log('total de gastos', total);
+      const dataExpenses = exps.map((expense) => ({
+        id: expense.id,
+        name: expense.description,
+        createdAt: format(new Date(expense.created_at), 'dd/MM/yyyy HH:mm'),
+        amount: expense.amount,
+        paid_by: expense.paid_by,
+      }));
+      return {
+        data: dataExpenses,
+        page,
+        lastPage: Math.ceil(total / limit),
+      };
+    } catch (error) {
+      throw new BadRequestException(
+        'Error fetching expenses: ' + error.message,
+      );
     }
   }
 }
